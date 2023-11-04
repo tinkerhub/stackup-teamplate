63bfc3fa527e8e718755e1e8ccf2fed8edcb601a
MUNNA <114686808+munna710@users.noreply.github.com>
2023-11-04 05:27:45 +0000

    k

--- ecommerce/backend/config/dbConnect.js
+++ ecommerce/backend/config/dbConnect.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
 
 const dbConnect = async () => {
-    try {
-        const conn = await mongoose.connect("mongodb://localhost:27017/ecommerce_db", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("Database Connected Successfully");
-    } catch (error) {
-        console.error("Error connecting to the database:", error.message);
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Database Connected Successfully");
+  } catch (error) {
+    console.error("Error connecting to the database:", error.message);
+  }
 };
 
 module.exports = dbConnect;
--- ecommerce/backend/index.js
+++ ecommerce/backend/index.js
@@ -3,14 +3,12 @@ const app = express();
 const dbConnect = require("./config/dbConnect");
 const dotenv = require("dotenv").config();
 const bodyParser = require('body-parser');
-const PORT = 4000;
+const PORT = 5000;
 const authRouter = require("./routes/authRoute");
 dbConnect();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/",(req , res)=>{
-    res.send("hello from server side");
-});
+
 app.use("/api/user", authRouter);
 app.listen(PORT, () => {
     console.log(`Server is running  at PORT ${PORT}`);
