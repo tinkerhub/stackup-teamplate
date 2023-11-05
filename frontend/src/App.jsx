e6af954c42b7f5ef2f578ca6968f9ae0b97155bd
Noel George C J <noelgeorge2004@gmail.com>
2023-11-05 19:36:00 +0530

    fixed bug of priority color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='bg-slate-200'>
       <p className='text-amber-400'>Hello</p>
-      <Task title="First task" deadline="21/01/2024" priority="3" />
+      <Task title="First task" deadline="21/01/2024" priority="1" />
     </div>
   )
 }
