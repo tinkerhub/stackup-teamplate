025dff41f153648cdb4250fc17eba89bf240efa4
Noel George C J <noelgeorge2004@gmail.com>
2023-11-06 23:08:37 +0530

    code cleanup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-slate-200'>
-      <p className='text-amber-400'>Hello</p>
+    <div className='bg-slate-200 h-screen'>
       <Task title="First task" deadline="21/01/2024" priority="1" />
     </div>
   )
---
+++ frontend/src/components/Project.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Project = () => {
+  return (
+    <div>
+        <div className='bg-orange-500 border-l-'>
+            
+        </div>
+        <div className='bg-orange-700'>
+
+        </div>
+        <div className='bg-orange-300'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Project
