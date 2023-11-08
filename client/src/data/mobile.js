c70ec23227be5eb2e4dd001fcb26efb09f483475
ktfawazhussain <ktfawazhussain@gmail.com>
2023-11-09 01:11:38 +0530

    different sections of home page added

---
+++ client/src/data/mobile.js
@@ -0,0 +1,14 @@
+
+const data ={
+    mobile: [{
+    name: "Motorola",
+    image: "../images/motorola.webpg"
+},
+{
+  name:"Television",
+  image: "./images/tv.webpg"
+}],
+electronics:[{name:"dfdfg",image:"fghffghf"}]
+};
+
+export default data
