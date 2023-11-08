992b12709778aa401ac99d0847fc5bc2a20a2e9e
AKA717 <alwinaji717@gmail.com>
2023-11-08 17:37:05 +0530

    merge mistake in user-route

--- back-end/routes/user-router.js
+++ back-end/routes/user-router.js
@@ -34,6 +34,114 @@ if (!fs.existsSync(profileDirectory))
     fs.mkdirSync(profileDirectory, { recursive: true });
 }
 
+// Add a new route to update a contact by ID
+router.put('/update-contact', async (req, res) => {
+    try {
+      const userId = req.body.userId;
+      const contactId = req.body.contactId;
+  
+      // Find the user profile by user ID
+      let userProfile = await Contact.findById(userId);
+  
+      if (!userProfile) {
+        return res.status(404).send(apiResponse({ message: 'User profile not found.' }));
+      }
+  
+      // Find the contact to update in the user's contacts
+      const contactToUpdate = userProfile.contacts.id(contactId);
+  
+      if (!contactToUpdate) {
+        return res.status(404).send(apiResponse({ message: 'Contact not found.' }));
+      }
+  
+      // Update the contact data with the request body
+      if (req.body.name) {
+        contactToUpdate.name = req.body.name;
+      }
+      if (req.body.email) {
+        contactToUpdate.email = req.body.email;
+      }
+      if (req.body.phone) {
+        contactToUpdate.phone = req.body.phone;
+      }
+      if (req.body.address) {
+        contactToUpdate.address = req.body.address;
+      }
+  
+      // Save the updated user profile
+      await userProfile.save();
+  
+      return res.send(apiResponse({ message: 'Contact updated successfully.', contact: contactToUpdate }));
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(apiResponse({ message: 'Error processing the request.' }));
+    }
+  });
+  
+
+// Add a new route to delete a contact by ID
+router.delete('/delete-contact', async (req, res) => {
+    try {
+      const userId = req.body.userId;
+      const contactId = req.body.contactId;
+
+      console.log(userId,contactId);
+  
+      // Find the user profile by user ID
+      let userProfile = await Contact.findById(userId);
+  
+      if (!userProfile) {
+        return res.status(404).send(apiResponse({ message: 'User profile not found.' }));
+      }
+  
+      // Find the index of the contact to delete
+      const contactIndex = userProfile.contacts.findIndex((contact) => contact._id.toString() === contactId);
+  
+      if (contactIndex === -1) {
+        return res.status(404).send(apiResponse({ message: 'Contact not found.' }));
+      }
+  
+      // Remove the contact from the user's profile
+      userProfile.contacts.splice(contactIndex, 1);
+  
+      // Save the updated user profile
+      await userProfile.save();
+  
+      return res.send(apiResponse({ message: 'Contact deleted successfully.' }));
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(apiResponse({ message: 'Error processing the request.' }));
+    }
+  });
+  
+
+router.get('/get-contacts',async (req,res) => {
+    
+    const userId = req.body.id;
+    const isUser = await Contact.findOne({ _id: userId });
+
+    console.log("contacts : ",isUser);
+
+    if(isUser !== null)
+    {
+        res.status(200).json({
+            success: true,
+            message: "success",
+            contacts : isUser.contacts
+        });
+    }
+    else
+    {
+        res.status(401).json(
+            {
+                success: false,
+                message: "user already exists"
+            }
+        )
+    }
+
+})
+
 // Image upload
 router.post('/add-contact', upload.single('image'), async (req, res) => {
 
