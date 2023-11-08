83af7a4da20231defb51e8de1c66473e9902afbc
Arjun Pratap <officiallyaninja@gmail.com>
2023-11-08 10:17:41 +0530

    print for where listeneing

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -96,6 +96,7 @@ async fn main() {
     // `axum::Server` is a re-export of `hyper::Server`
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     // http://127.0.0.1:3000
+    println!("listening on {}", addr);
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
