c8dc179eea1c48bc4f58d8b27164c336a13ebe4e
midhundas27 <dasmidhun27@gmail.com>
2023-11-09 23:42:54 +0530

    made vertically responsive on some pages

--- client/src/data/mobile.js
+++ client/src/data/mobile.js
@@ -2,6 +2,8 @@ import  image1 from '../images/motorola.webp'
 import image2 from '../images/not.webp'
 import image3 from '../images/realme.webp'
 import image4 from '../images/iphone15.webp'
+import image5 from '../images/s22.webp'
+import image6 from '../images/pixel.webp'
 
 const mobile = [{
         name: "Motorola",
@@ -18,6 +20,38 @@ const mobile = [{
     {
         name: "Iphone 15",
         image: image4
+    },
+    {
+      name: "Samsung S22",
+      image: image5
+    },
+    {
+      name: "Google Pixel 7a",
+      image: image6
+    },
+    {
+      name: "Motorola",
+      image: image1
+    },
+    {
+      name: "Nothing",
+      image: image2
+    },
+    {
+    name: "Realme",
+    image: image3
+    },
+    {
+      name: "Iphone 15",
+      image: image4
+    },
+    {
+    name: "Samsung S22",
+    image: image5
+    },
+    {
+    name: "Google Pixel 7a",
+    image: image6
     }
     ];
 
