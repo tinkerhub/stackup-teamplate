b385936196500fd72344b4528789cfb3b6c0fbcd
AKA717 <alwinaji717@gmail.com>
2023-11-10 21:24:06 +0530

    search feature addded

--- front-end/src/components/view/view.jsx
+++ front-end/src/components/view/view.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import Card from "../card/card";
-import { contactFetchAction } from "../../redux/actions/actions";
+import { contactFetchAction} from "../../redux/actions/actions";
 import "./view.css";
 
 const UserView = ({ user ,fetchContacts}) => {
   const navigate = useNavigate();
+  const [searchInput, setSearchInput] = useState("");
 
   useEffect(() => {
     if (!user.login) {
@@ -21,7 +22,12 @@ const UserView = ({ user ,fetchContacts}) => {
       fetchContacts(userId);
     }
 
-  }, [user.login, navigate]);
+    if(user.contact_add)
+    {
+      user.contact_add = false;
+    }
+
+  }, [user.login, navigate,user.contact_add]);
 
   if(user.delete_contact)
   {
@@ -35,31 +41,56 @@ const UserView = ({ user ,fetchContacts}) => {
     user.edit_contact = false;
   }
 
+  const filteredContacts = user.contacts
+    ? user.contacts.filter((item) =>
+        item.name.toLowerCase().includes(searchInput.toLowerCase())
+      )
+    : [];
+
+  const handleSearchInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
   return (
     <div className="container view-container">
       <div className="row search-view">
         <div className="col-md-6 mx-auto">
           <div className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="Search" />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search"
+              onChange={handleSearchInputChange}
+              value={searchInput}
+            />
             <div className="input-group-append">
-              <button className="btn btn-primary search-btn" type="button">
-                Search
-              </button>
+            <Link to="/add-contact" className="btn btn-success btn-block search-btn">
+            Add Contact
+          </Link>
             </div>
           </div>
-          <div className="col-md-6 mx-auto">
-          <Link to="/add-contact" className="btn btn-success btn-block">Add Contact</Link>
-        </div>
         </div>
       </div>
-      {user.contacts && user.contacts.length > 0 ? (
-        user.contacts.map((item) => (
-          <Card key={item._id} name={item.name} phone={item.phone} userId={user.user.id} contactId={item._id} email={item.email} address={item.address}/>
-        ))
-      ) : (
-        <p>No contacts available</p>
-      )}
+      <div className="row col-md-12">
+        {filteredContacts && filteredContacts.length > 0 ? (
+          filteredContacts.map((item) => (
+            <div key={item._id} className="col-md-6 mb-2">
+              <Card
+                name={item.name}
+                phone={item.phone}
+                userId={user.user.id}
+                contactId={item._id}
+                email={item.email}
+                address={item.address}
+              />
+            </div>
+          ))
+        ) : (
+          <p>No contacts available</p>
+        )}
       </div>
+    </div>
+
   );
 };
 
